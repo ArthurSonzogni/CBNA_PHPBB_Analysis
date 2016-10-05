d37fc993691689e416c2211afe24deea14768ee5
ArthurSonzogni <Arthur.Sonzogni@grenoble-inp.org>
2016-10-06 00:06:11 +0200

    Transform the CBNA exporter to fit the last update format.

--- src/transform/to_test_cbna_forum.cpp
+++ src/transform/to_test_cbna_forum.cpp
@@ -12,8 +12,6 @@ static bool mkdir(const std::string& directory)
 
 void to_test_cbna_forum(const RawForum& forum, const std::string& directory)
 {
-    std::map<std::string,int> category_id_map;
-
     //-------------
     // Topics
     //-------------
@@ -22,31 +20,10 @@ void to_test_cbna_forum(const RawForum& forum, const std::string& directory)
         auto& topic_id = topic_pair.first;
         auto& topic    = topic_pair.second;
 
-        int& category_id = category_id_map[topic.path[2]];
-        bool create_category = false;
-        if (!category_id)
-        {
-            category_id = category_id_map.size();
-            create_category = true;
-        }
-
-        std::string category_directory =
-            directory + "/message_board/contents/categories/" +
-            std::to_string(category_id);
-        std::string topic_directory = category_directory +
+        std::string topic_directory =
+            directory + "/message_board/contents" +
              "/topics/" + std::to_string(topic_id);
         mkdir(topic_directory);
-
-        //---------------------
-        // Making new category
-        //---------------------
-        if (create_category)
-        {
-            JSON json;
-            json["description"] = "";
-            json["name"] = topic.path[2];
-            std::ofstream(category_directory+"/info") << std::setw(4) << json;
-        }
 
         //-------------
         // Topic info
@@ -69,8 +46,7 @@ void to_test_cbna_forum(const RawForum& forum, const std::string& directory)
             JSON json;
             json["authors"].push_back(0);
             json["date"] = "2016-01-01T00:00:00";
-            //json["date"] = message.date.to_string();
-            json["text"] = message.content;
+            json["content"] = message.content;
             std::ofstream file(topic_directory +
                 "/messages/" + std::to_string(message_id++));
             file << std::setw(4) << json;
